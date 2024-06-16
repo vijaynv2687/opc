86f5829ebe546c2652cd1f7dbc2d3dbca8b04d90
vijay n v <83916182+vijaynv2687@users.noreply.github.com>
2024-06-16 21:31:30 +0530

    Update compartment.tf

--- compartment.tf
+++ compartment.tf
@@ -1,5 +1,5 @@
 resource "oci_identity_compartment" "VJ_P" {
-  compartment_id = "ocid1.tenancy.oc1..aaaaaaaabqz5tqblgsr7qa4dbwi7h6pbnf4ifnvo32sj4z6y2gjc4afi7i3a"
+  compartment_id = "ocid1.tenancy.oc1..aaaaaaaa..........."
   description    = "Compartment for Terraform resources."
   name           = "ATF1234"
 }
